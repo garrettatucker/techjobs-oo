8aabeed56806a62f0d123936115e0b9303a4da23
Garrett Tucker <garrettatucker@gmail.com>
2019-01-24 19:02:32 -0600

    Finished assignment

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,5 +1,6 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Job;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.naming.Name;
 import javax.validation.Valid;
 
 /**
@@ -24,7 +26,8 @@ public class JobController {
     public String index(Model model, int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
-
+        Job newJob = jobData.findById(id);
+        model.addAttribute("newJob", newJob);
         return "job-detail";
     }
 
@@ -34,14 +37,25 @@ public class JobController {
         return "new-job";
     }
 
+    // TODO #6 - Validate the JobForm model, and if valid, create a
+    // new Job and add it to the jobData data store. Then
+    // redirect to the job detail view for the new Job.
+
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @Valid JobForm jobForm, Errors errors) {
+    public String add(Model model, @Valid JobForm jobForm, Job newJob, Errors errors) {
 
-        // TODO #6 - Validate the JobForm model, and if valid, create a
-        // new Job and add it to the jobData data store. Then
-        // redirect to the job detail view for the new Job.
+        newJob = new Job(jobForm.getName(),jobData.getEmployers().findById(jobForm.getEmployerId()),
+                jobData.getLocations().findById(jobForm.getLocationId()),
+                jobData.getPositionTypes().findById(jobForm.getPositionTypeId()),
+                jobData.getCoreCompetencies().findById(jobForm.getcoreCompetencyId()));
 
-        return "";
+        if (errors.hasErrors()){
+            model.addAttribute(jobForm);
+            return "new-job";
+        }
+        int x = newJob.getId();
+        jobData.add(newJob);
+        return "redirect:?id="+x;
 
     }
 }
--- src/main/java/org/launchcode/models/forms/JobForm.java
+++ src/main/java/org/launchcode/models/forms/JobForm.java
@@ -22,11 +22,21 @@ public class JobForm {
     @NotNull
     private int employerId;
 
+    @NotNull
+    private int locationId;
+
+    @NotNull
+    private int positionTypeId;
+
+    @NotNull
+    private int coreCompetencyId;
+
     /*
         TODO #3 - Included other fields needed to create a job,
         with correct validation attributes and display names.
         Don't forget to add getters and setters
      */
+
 
     private ArrayList<Employer> employers;
     private ArrayList<Location> locations;
@@ -42,7 +52,9 @@ public class JobForm {
         */
 
         employers = jobData.getEmployers().findAll();
-
+        locations = jobData.getLocations().findAll();
+        coreCompetencies = jobData.getCoreCompetencies().findAll();
+        positionTypes = jobData.getPositionTypes().findAll();
     }
 
     public String getName() {
@@ -59,6 +71,30 @@ public class JobForm {
 
     public void setEmployerId(int employerId) {
         this.employerId = employerId;
+    }
+
+    public int getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(int locationId) {
+        this.locationId = locationId;
+    }
+
+    public int getPositionTypeId() {
+        return positionTypeId;
+    }
+
+    public void setPositionTypeId(int positionTypeId) {
+        this.positionTypeId = positionTypeId;
+    }
+
+    public int getcoreCompetencyId() {
+        return coreCompetencyId;
+    }
+
+    public void setcoreCompetencyId(int coreCompetencieId) {
+        this.coreCompetencyId = coreCompetencieId;
     }
 
     public ArrayList<Employer> getEmployers() {
